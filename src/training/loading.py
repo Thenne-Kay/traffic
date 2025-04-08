a5773c01bac336acfefec51a62b11c0ba0a6f696
thenne-kay <thenne.kay@gmail.com>
2025-04-08 14:00:09 +0300

    changed loading

--- src/training/loading.py
+++ src/training/loading.py
@@ -29,49 +29,15 @@ def store_waiting_times(env, label):
     - env: Environment instance (with waiting_time_history)
     - label: Unique identifier for this dataset
     """
-    
-    for times in  np.array(env.waiting_time_history):
-        run.log(label,times)
-   
+
+    run.log_table(
+         label,
+         np.array(env.waiting_time_history)
+    )
+  
 
 
 x = SumoTrafficEnv(config_file, net_file, route_file, False, 40001)
-obs, info = x.reset()
-# Run 10 steps
-
-
-model = PPO.load(model_path, env=x)
-for step in range(4000):
-    # Action selection - random for demonstration
-    # In practice, replace this with your model's action selection
-    # Random action (0 or 1)
-    action, _states = model.predict(obs, deterministic=True)
-    # Take a step
-    obs, reward, terminated, truncated, info = x.step(action)
-    # obs, reward, terminated, truncated, info = y.step(action2)
-    if (len(x.traffic_lights) > 0) and terminated == False:
-        tl_status = x.get_traffic_light_status()
-
-        for tl_id in x.traffic_lights:
-            try:
-                lanes = traci.trafficlight.getControlledLanes(tl_id)
-            except:
-                print("- Could not retrieve controlled lanes")
-    else:
-        print("\nNo traffic lights found in the simulation")
-        # Check if episode ended
-        if terminated or truncated:
-            print("\nEpisode ended early!")
-            # Close the environment
-            x.close()
-            # y.close()
-            print("\nSimulation complete.")
-            break
-
-store_waiting_times(x, "trained")
-
-
-# x = SumoTrafficEnv(config_file, net_file, route_file, False, 40001)
 obs, info = x.reset()
 
 action = np.array([0, 0, 0, 0, 0], dtype=np.int8)
@@ -103,4 +69,34 @@ for step in range(4000):
             break
 store_waiting_times(x, "untrained")
 
+x2 = SumoTrafficEnv(config_file, net_file, route_file, False, 40001)
+obs, info = x2.reset()
 
+model = PPO.load(model_path, env=x2)
+for step in range(4000):
+    # Action selection - random for demonstration
+    # In practice, replace this with your model's action selection
+    # Random action (0 or 1)
+    action, _states = model.predict(obs, deterministic=True)
+    # Take a step
+    obs, reward, terminated, truncated, info = x2.step(action)
+    # obs, reward, terminated, truncated, info = y.step(action2)
+    if (len(x2.traffic_lights) > 0) and terminated == False:
+        tl_status = x2.get_traffic_light_status()
+
+        for tl_id in x2.traffic_lights:
+            try:
+                lanes = traci.trafficlight.getControlledLanes(tl_id)
+            except:
+                print("- Could not retrieve controlled lanes")
+    else:
+        print("\nNo traffic lights found in the simulation")
+        # Check if episode ended
+        if terminated or truncated:
+            print("\nEpisode ended early!")
+            # Close the environment
+            x2.close()
+            # y.close()
+            print("\nSimulation complete.")
+            break
+store_waiting_times(x2, "trained")
